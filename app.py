9047f516a06e02a85307a4854463a748393186e8
nickchacha <nickchacha@gmail.com>
2022-12-03 18:13:33 +0300

    Creating routes outsides the common folder

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 from config import Config
+from posts.Blueprint import posts
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.register_blueprint(posts, url_prefix='/blog')
 
